4ce75691c7e8b87052d3e011bee7b0a9430c83a2
chinchana k <chinchanak640@gmail.com>
2025-03-28 10:48:58 +0530

    pendrive.sh

--- linux/pendrive.sh
+++ linux/pendrive.sh
@@ -3,7 +3,7 @@ cp *.c *.py /tmp/backup/
 cd /tmp 
 tar -czvf Backup-2025-03-28.gz -C /tmp backup
 udisksctl mount -b /dev/sda1
-mv Backup-2025-03-28.gz /media/rvu/shadow
+mv Backup-2025-03-28.gz /media/rvu/ACER UFD
 udisksctl unmount -b /dev/sda1
 rm -r /tmp/backup
 
