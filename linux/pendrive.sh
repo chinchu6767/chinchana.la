f15480f98653afad3fe8f780b0a0d08073580448
chinchana k <chinchanak640@gmail.com>
2025-03-28 10:47:12 +0530

    Add files via upload

---
+++ linux/pendrive.sh
@@ -0,0 +1,15 @@
+mkdir -p /tmp/backup
+cp *.c *.py /tmp/backup/
+cd /tmp 
+tar -czvf Backup-2025-03-28.gz -C /tmp backup
+udisksctl mount -b /dev/sda1
+mv Backup-2025-03-28.gz /media/rvu/shadow
+udisksctl unmount -b /dev/sda1
+rm -r /tmp/backup
+
+
+
+
+
+
+
